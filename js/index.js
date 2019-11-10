3d92424a3f8b886b29937f182b4408d3fcf93e2a
Breellz <barseetbrown@gmail.com>
2019-11-10 07:16:29 +0100

    added a popup on clicking the submit button,add an animation in the header

--- js/index.js
+++ js/index.js
@@ -1,4 +1,17 @@
-const handleSubmit = (e) =>{
-    e.preventDefault();
-} 
- 
+window.onload = function () {
+    setInterval(loadIntro, 2000)
+  }
+  
+  var intros = ["a front end developer", "a statistician", "a Gamer", "a Swimmer", "a Gym Freak", "a Tech Lover", "a Movie enthusiast"]
+  var index = 0;
+  var intro = document.querySelector("#intro-loader")
+  
+  function loadIntro() {
+    intro.innerHTML = intros[index];
+    intro.classList.add("fade");
+    index++;
+    if (index >= intros.length) {
+      index = 0;
+    }
+  }
+  
